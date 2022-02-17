23ffbc52f048a6304c847a5b185d6c68e7ed0351
kabir <makkobir79@gmail.com>
2022-02-17 21:59:44 +0600

    add

--- js/app.js
+++ js/app.js
@@ -1,28 +1,16 @@
-// assign the varible 
-/* const myIncome = document.getElementById('my-income');
-const food = document.getElementById('food-input');
-const rent = document.getElementById('rent-input');
-const other = document.getElementById('other-input');
-const expenses = document.getElementById('total-expenses');
-const myBalance = document.getElementById('total-balance'); */
-
-
-
+// take input value 
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputText = inputField.value;
     const inputAmount = parseInt(inputText);
+    // Error handle 
+    if (inputField.value < 0) {
+        alert('please enter a valid number')
+    }
     inputField.value = '';
     return inputAmount;
 }
-
-// function 2
-// function updateTotal(updateTotal) {
-//     const balanceTotal = document.getElementById('my-income');
-//     const balanceText = balanceTotal.value;
-//     const balanceAmount = parseInt(balanceText);
-// }
-
+// event handler 
 document.getElementById('calculater').addEventListener('click', function () {
     const food = getInputValue('food-input')
     const rent = getInputValue('rent-input')
@@ -34,31 +22,12 @@ document.getElementById('calculater').addEventListener('click', function () {
     const myIncomeText = myIncome.value;
     const myIncomeTotal = parseInt(myIncomeText);
     const balance = document.getElementById('total-expenses').innerText = myIncomeTotal;
-    const setBalance = document.getElementById('total-balance').innerText = balance - totalCalculation;
-    // setBalance.innerText = totalCalculation - setBalance;
-
-
+    const setBalance = document.getElementById('total-balance')
+    setBalance.innerText = balance - totalCalculation;
 });
-// event handaler
-/* document.getElementById('calculater').addEventListener('click', function () {
-    // facth the value and parse this to a number 
-    const newFood = parseInt(food.value);
-    const newRent = parseInt(rent.value);
-    const newOther = parseInt(other.value);
-    const newIncome = parseInt(myIncome.value);
-    // set and Append
-    const totalIncome = newIncome;
-    const totalExpenses = newFood + newRent + newOther;
-    expenses.innerText = totalExpenses;
 
-    // error massages 
+// saving part 
+document.getElementById('save-btn').addEventListener('click', function () {
+    console.log('clicked')
+})
 
-    // minus balance
-    const balance = totalIncome - totalExpenses;
-    myBalance.innerText = balance;
-
-    // clear input fild 
-    food.value = '';
-    rent.value = '';
-    other.value = '';
-}); */
