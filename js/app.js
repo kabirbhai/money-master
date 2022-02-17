1690242bf3a9f6840c00ffe22df3dd33e8a44992
kabir <makkobir79@gmail.com>
2022-02-17 22:42:06 +0600

    finish

--- js/app.js
+++ js/app.js
@@ -3,12 +3,9 @@ function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputText = inputField.value;
     const inputAmount = parseInt(inputText);
-    // Error handle 
-    if (inputField.value < 0) {
-        alert('please enter a valid number')
-    }
     inputField.value = '';
     return inputAmount;
+
 }
 // event handler 
 document.getElementById('calculater').addEventListener('click', function () {
@@ -17,6 +14,7 @@ document.getElementById('calculater').addEventListener('click', function () {
     const other = getInputValue('other-input')
     const totalCalculation = food + rent + other;
 
+
     // withOut function 
     const myIncome = document.getElementById('my-income');
     const myIncomeText = myIncome.value;
@@ -24,6 +22,14 @@ document.getElementById('calculater').addEventListener('click', function () {
     const balance = document.getElementById('total-expenses').innerText = myIncomeTotal;
     const setBalance = document.getElementById('total-balance')
     setBalance.innerText = balance - totalCalculation;
+
+    // Error handle 
+    if (totalCalculation.value < 0) {
+        alert('Please enter a valid number grater than 0')
+    }
+    else if (totalCalculation.value != Number) {
+        alert('Please enter a valid number')
+    }
 });
 
 // saving part 
